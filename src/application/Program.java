f0a4920924bdb60df18b7c12bed3454d493ad849
alefagundes <alefagundes2000.af@gmail.com>
2021-10-06 13:01:20 -0300

    Using TreeSet with method compareTo orther by name

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,28 +1,23 @@
 package application;
 
-import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
+
+import entities.Product;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
-		Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
-//union
+		Set<Product> set = new TreeSet<>();
 		
-		Set<Integer> c = new TreeSet<>(a);
-		c.addAll(b);
-		System.out.println(c);
-//intersection
+		set.add(new Product("TV", 900.0));
+		set.add(new Product("Notebook", 1200.0));
+		set.add(new Product("Tablet", 400.0));
 		
-		Set<Integer> d = new TreeSet<>(a);
-		d.retainAll(b); /*sao os elementos em comum no cunjunto*/
-		System.out.println(d);
-//difference
+
+		for(Product p: set) {
+			System.out.println(p);
+		}
 		
-		Set<Integer> e = new TreeSet<>(a);
-		e.removeAll(b);
-		System.out.println(e);
 	}
 }
